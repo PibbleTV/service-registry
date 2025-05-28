909d38f0740e7b9b53d0d323a5df4e0e055fda26
TanzerDx <hristoganchev3@gmail.com>
2025-05-28 23:32:16 +0200

    test

--- src/main/java/com/pibbletv/service_registry/SecurityConfig.java
+++ src/main/java/com/pibbletv/service_registry/SecurityConfig.java
@@ -3,25 +3,27 @@ package com.pibbletv.service_registry;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
-import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.web.server.SecurityWebFilterChain;
-
-
-import static org.springframework.security.config.Customizer.withDefaults;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
 
 
 @Configuration
-@EnableWebFluxSecurity
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
-    SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
-        return http
-                .authorizeExchange(exchanges -> exchanges
-                        .anyExchange().authenticated()
-                )
-                .httpBasic(withDefaults())
-                .csrf(ServerHttpSecurity.CsrfSpec::disable).build();
+    public SecurityFilterChain securityWebFilterChain(HttpSecurity http) throws Exception {
+
+        http
+                .csrf(CsrfConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/eureka/**").authenticated()
+                        .anyRequest().authenticated()
+                ).httpBasic(Customizer.withDefaults());
+
+        return http.build();
     }
 }
